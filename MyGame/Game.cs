ff3bd4453325932b56438e8f6a9ba77a9afe9331
Denis Lystsev <dlyst@mail.ru>
2019-03-25 14:28:33 +0300

    Начал создавать класс Game

--- MyGame/Game.cs
+++ MyGame/Game.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace MyGame
 {
-    class Game
+    static class Game
     {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+        //Свойства игрового поля
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        static Game()
+        {
+
+        }
+
+        public static void Init(Form form)
+        {
+
+        }
     }
 }
